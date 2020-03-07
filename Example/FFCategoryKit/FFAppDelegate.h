8dfd44028c9665a695c42f67688a243f42420e39
Banning <banning.cai@dingtone.me>
2020-03-07 16:10:01 +0800

    Initial commit

---
+++ Example/FFCategoryKit/FFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FFAppDelegate.h
+//  FFCategoryKit
+//
+//  Created by Banning on 03/07/2020.
+//  Copyright (c) 2020 Banning. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FFCategoryKit/FFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFViewController.h
+//  FFCategoryKit
+//
+//  Created by Banning on 03/07/2020.
+//  Copyright (c) 2020 Banning. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFViewController : UIViewController
+
+@end
